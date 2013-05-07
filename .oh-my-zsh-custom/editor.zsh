682f8b245dbf0f699c5c26052b61be340c207e63
Morton Jonuschat <m.jonuschat@gute-botschafter.de>
2013-05-07 10:18:36 +0200

    Sublime Text command line alias

--- .oh-my-zsh-custom/editor.zsh
+++ .oh-my-zsh-custom/editor.zsh
@@ -2,3 +2,4 @@
 export EDITOR=/usr/bin/vim
 alias e='/usr/local/bin/mvim .'
 alias s='/usr/local/bin/sublime -n .'
+alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl -n $*'
