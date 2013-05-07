25614d98a1be9f71c9d633de0a71963d45c8a54c
Morton Jonuschat <m.jonuschat@gute-botschafter.de>
2013-05-07 10:18:23 +0200

    Updated search paths

--- .oh-my-zsh-custom/auto_cd.zsh
+++ .oh-my-zsh-custom/auto_cd.zsh
@@ -1,3 +1,3 @@
 setopt auto_cd
-cdpath=(. ~/Documents/Development /Volumes/Daten/Development/gubo /Volumes/Daten/Development/mojo /Volumes/7\ Web/www.projektstatus.de /Volumes/Daten/Development/gubo/dwag )
+cdpath=(. ~/Documents/Development /Volumes/Daten/Development/gubo /Volumes/Daten/Development/mojo /Volumes/7\ Web/ /Volumes/Daten/Development/gubo/dwag )
 
